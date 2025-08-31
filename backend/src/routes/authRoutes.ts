2952a4eae5361007ea91f1e4c653a6d286db434b
manveethjain8 <manveethjain@gmail.com>
2025-08-31 17:58:26 +0530

    Created auth Controller and routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { callback_from_google, redirect_to_google } from '../controllers/accountsController'
+import { callback_from_google, redirect_to_google } from '../controllers/authController'
 
 const router = express.Router()
 
 // Routes to access the authentication
 router.get('/google', redirect_to_google)
 router.get('/google/callback', callback_from_google)
+
 
 export default router
