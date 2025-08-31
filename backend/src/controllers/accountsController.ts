d5386f81dfb850883d42d61bd47880f7030a9d0c
manveethjain8 <manveethjain@gmail.com>
2025-08-31 11:22:31 +0530

    Corrected the redirect path

--- backend/src/controllers/accountsController.ts
+++ backend/src/controllers/accountsController.ts
@@ -10,11 +10,10 @@ export const redirect_to_google = await passport.authenticate('google', {
 // Receives the call back from google and decides what to do based on the received result
 export const callback_from_google = [passport.authenticate('google', {
         failureRedirect: '/',
-        successRedirect: '/home'
     }),
     (req: AuthenticatedRequestInterface, res: Response) => {
         try{
-            console.log("Login Successfull")
+            res.redirect('http://localhost:5173/home')
         }catch(err: unknown){
             if(err instanceof Error){
                 res.status(500).json({message: 'Error logging in from Google', error: err.message})
